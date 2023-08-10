610f46fa71b7dc21a0d7a452da422cc48e0f0b48
hotta822 <126322004+hotta822@users.noreply.github.com>
2023-08-10 18:37:04 +0900

    complete node app

--- node-app/scr/index.js
+++
@@ -1,4 +0,0 @@
-var sayHello = function (name) {
-    return "Hello, " + name + "!";
-};
-console.log(sayHello("Michael Jackson"));
--- node-app/scr/index.ts
+++
@@ -1,4 +0,0 @@
-const sayHello = (name: string) => {
-  return `Hello, ${name}!`;
-};
-console.log(sayHello("Michael Jackson"));
