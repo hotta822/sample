856530175c895458f2a5aabe5be58f142542b617
hotta822 <126322004+hotta822@users.noreply.github.com>
2023-08-12 22:06:45 +0900

    in progress to create browser app

--- browser-app/ts/index.ts
+++ browser-app/ts/index.ts
@@ -1,20 +1,17 @@
 import {EventListener} from "./EventListener"
 
 class Application {
-    start(){
-        const eventlistener = new EventListener
-        const button = document.getElementById("deleteAllDoneTask")
-        
-        if(!button)return
-        
-        eventlistener.add(
-            "sample",
-            "click",
-            button,
-            ()=>alert("clicked"),
-        )
+    private readonly eventListener = new EventListener
 
-        eventlistener.remove("sample")
+    start(){
+        const createForm = document.getElementById("createForm") as HTMLElement //タスクを作成するためにHTMLのidがcreateFormの要素を取得する
+        
+        this.eventListener.add("submit-handler","submit",createForm,this.handleSubmit) //id名,フォームを送信するイベント,要素の指定,行う処理
+    }
+    
+    private handleSubmit = (e:Event) =>{
+        e.preventDefault() //submitのデフォルトに設定されているform送信先が指定されていない場合、現在のURLに対してフォームの送信を行う処理を防ぐ(prevent)ようにする
+        console.log("submitted")
     }
 }
 
